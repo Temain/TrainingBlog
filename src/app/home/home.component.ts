dcdec7b86b90994760a5b2b644fda6919adf27a4
temain <asv.artm@gmail.com>
2018-11-08 02:16:24 +0300

    Верстка

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  postImageHeight: number;
+
+  constructor(private element: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.equalizeHeight('.featured-posts .text', '.featured-posts .image');
   }
 
+  ngAfterViewInit() {
+  }
+
+  equalizeHeight(text: string, image: string) {
+    this.postImageHeight = this.element.nativeElement.querySelector(text).offsetHeight;
+  }
 }
